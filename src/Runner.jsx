7c9f04f5be982c20b722fc1f1dd8bce89cf0d3cb
tutyamxx <15928925+tutyamxx@users.noreply.github.com>
2026-01-24 12:04:58 +0000

    UPDATE: OCD Tweaks in Runner.jsx

--- src/Runner.jsx
+++ src/Runner.jsx
@@ -105,15 +105,17 @@ const Runner = ({ pkg, initialCode }) => {
         const importLines = imports.map(({ packageName, specifier }) => {
             const trimmedSpecifier = specifier?.trim();
             const encodedPackage = encodeURIComponent(packageName);
-            const safeSpecifierName = specifier.replace(/\W/g, '_');
+            const safeSpecifierName = specifier?.replace(/\W/g, '_');
 
             // --| Helper snippet for dynamic import with Skypack + esm.sh fallback
             const importWrapper = (varName) => `
                 let ${varName};
+
                 try {
                     ${varName} = await import('https://cdn.skypack.dev/${encodedPackage}');
                 } catch (err) {
                     console.error('[Package Error]', '${packageName}', err.message || err, '⚠️ This package might be CommonJS or use Node-only APIs.');
+
                     try {
                         ${varName} = await import('https://esm.sh/${encodedPackage}@latest?bundle');
                     } catch (err2) {
